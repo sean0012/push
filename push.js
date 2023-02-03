2ae12984a70b0a242c6cce1b52ad4fddc3e95fb0
sun <leesunwoo@Lees-MacBook-Pro.local>
2023-02-03 15:11:58 +0900

    3

--- push.js
+++ push.js
@@ -7,6 +7,13 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@code', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {"endpoint:": "https://updates.push.services.mozilla.com/wpush/v1/gAAAAABj3KTaQoLWA5qrpI1dqwMGEePtSJZOhraP7JA-2kN2dYEberRU1dmdB6iNffyjfGzz-V_28F59600zjSCvdpvTw-KgYQze7pgUKXOuAS7L2Wh3j1ZvmbPfmq-gxvc2CXpy2ZEh", "expirationTime":null,"keys":{"auth":"6aSqYbSjrxFNHZ85igMI5Q","p256dh":"BG6BZPVFsGiJEbF60F4xV6BqHzAsRVKoihT5QM0VUyKDmSfpJv_bwE3Llu-fSQHsf0oTCyfu8Cu0XYhRzmpPyOA"}}
+let sub = {
+	"endpoint:": "https://updates.push.services.mozilla.com/wpush/v1/gAAAAABj3KTaQoLWA5qrpI1dqwMGEePtSJZOhraP7JA-2kN2dYEberRU1dmdB6iNffyjfGzz-V_28F59600zjSCvdpvTw-KgYQze7pgUKXOuAS7L2Wh3j1ZvmbPfmq-gxvc2CXpy2ZEh",
+	"expirationTime":null,
+	"keys":{
+		"auth":"6aSqYbSjrxFNHZ85igMI5Q",
+		"p256dh":"BG6BZPVFsGiJEbF60F4xV6BqHzAsRVKoihT5QM0VUyKDmSfpJv_bwE3Llu-fSQHsf0oTCyfu8Cu0XYhRzmpPyOA"
+	}
+};
 
 push.sendNotification(sub, 'test message');
