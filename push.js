59c0d33326afa84dba978f4e8872b1c985329b6d
sun <leesunwoo@Lees-MacBook-Pro.local>
2023-02-03 13:50:41 +0900

    fix

--- push.js
+++ push.js
@@ -7,6 +7,13 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@code', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
+let sub = {
+	"endpoint":"https://updates.push.services.mozilla.com/wpush/v1/gAAAAABj3…9TYKCU5tNqZ3Q8x4_LVgNnIWLM3C9WzA5u31vXL7w_3URDabaNZInQY3pcQT",
+	"expirationTime":null,
+	"keys":{
+		"auth":"pg4N9No_dwavo43nQTuELA",
+		"p256dh":"BJMZkTrC9kI-Gt70V-25ZcomB4TcW08HVc_8gXtqOZWhG1P3mL8Mu-OBg1AnyelqZDS97evCGW1ZurCI4e-NgCY"
+	}
+};
 
 push.sendNotification(sub, 'test message');
